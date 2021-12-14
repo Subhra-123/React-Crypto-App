c9b82c6705273c485cebf2f87536ac4524941aaa
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2021-12-14 13:27:54 +0530

    initial commit/Created homepage

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+var options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/exchanges',
+    headers: {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': '7cb424dc0emshe4eca59cc525143p1f5f86jsn4c1ba54a3141'
+    }
+  };
