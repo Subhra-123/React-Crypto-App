b7b1cc627a8fe9ba71d22078ef93efa8474da0cc
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2021-12-14 18:37:33 +0530

    Created Crypto-Card

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,22 @@
-var options = {
-    method: 'GET',
-    url: 'https://coinranking1.p.rapidapi.com/exchanges',
-    headers: {
-      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+const cryptoApiHeaders={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
       'x-rapidapi-key': '7cb424dc0emshe4eca59cc525143p1f5f86jsn4c1ba54a3141'
-    }
-  };
+}
+
+const baseUrl='https://coinranking1.p.rapidapi.com';
+const createRequest=(url)=>({url,headers:cryptoApiHeaders});
+
+export const cryptoApi=createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl }),
+    endpoints:(builder)=>({
+        getCryptos:builder.query({
+            query:()=>createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+}=cryptoApi
